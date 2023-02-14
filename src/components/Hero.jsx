2e6af74b135b8c44a07536b90de5ee748ccf74e4
StepanusRio <stepanusriodefaardian@gmail.com>
2023-02-14 12:22:10 +0700

    Fix Download CV Button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
             <h2 className='font-medium text-stroke/70 mb-4 lg:text-2xl'><span ref={el}></span></h2>
             <p className='font-medium text-secondary mb-10 leading-relaxed'>Hello, I am a web developer with a passion for creating beautiful, functional, and user-friendly websites. I have a strong background in both front-end and back-end development and have experience working with a variety of technologies, including HTML, CSS, JavaScript, and PHP.</p>
             <Link to='portofolio' duration={500} smooth className='text-main text-base font-semibold bg-primary py-3 px-8 rounded-full hover:shadow-lg cursor-pointer'>Portfolio</Link>
-            <a href="/public/Cv-StepanusRioDefaArdiantoro.pdf" className='py-2 px-3 border border-slate-500 ml-3 rounded-md hover:border-primary hover:text-primary' download>Download CV</a>
+            <a href="Cv-StepanusRioDefaArdiantoro.pdf" className='py-2 px-3 border border-slate-500 ml-3 rounded-md hover:border-primary hover:text-primary' download>Download CV</a>
           </div>
           <div className="w-full self-end px-4 lg:w-1/2">
             <div data-aos='fade-up' data-aos-duration='3000' className="mt-10 relative lg:mt-0 lg:right-0">
